0af853265e1119b03b7fe03197db87e50760ff23
yahyaaaz <46232918+yahyaaaz@users.noreply.github.com>
2019-01-03 06:38:34 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,8 @@
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    client.user.setGame(الكلام الي تبيه يطلع لك في الحاله,"http://twitch.tv/y04zgamer%22)
+    client.user.setStatus("dnd")
+  console.log(Logged in as ${MzI2NTI3Nzk4MTY2NTUyNTg2.Dw-voA.ywDx-YNGAsyvYFQ37ER3TcIGafA}!);
